bfbd83ae21b97e9efc49c0a11c1fdd53b424628d
eduardovaillant <eduardo.bvaillant@gmail.com>
2020-11-30 19:34:14 -0300

    refactor: connect to mongodb before integration tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,9 +1,21 @@
 import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
 
-jest.useFakeTimers()
-
 describe('Sign Up Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   test('Should return an account on succes', async () => {
     await request(app)
       .post('/api/signup')

13b4777a6601b54091ec98b2384a1371a4e9ac6f
eduardovaillant <eduardo.bvaillant@gmail.com>
2020-11-30 16:08:52 -0300

    refactor: chage cors test

--- src/main/middlewares/cors.test.ts
+++ src/main/middlewares/cors.test.ts
@@ -3,7 +3,7 @@ import app from '../config/app'
 
 describe('Cors Middleware', () => {
   test('Should enable cors', async () => {
-    app.post('/test_cors', (req, res) => {
+    app.get('/test_cors', (req, res) => {
       res.send(req.body)
     })
     await request(app)

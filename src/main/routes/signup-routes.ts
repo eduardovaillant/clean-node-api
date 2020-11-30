ec1ec9172e166820659acae576c4ade03f1a3fb9
eduardovaillant <eduardo.bvaillant@gmail.com>
2020-11-30 20:13:31 -0300

    refactor: remove eslint rule

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -3,6 +3,5 @@ import { adptRoute } from '../adapters/express-route-adapter'
 import { makeSignUpController } from '../factories/signup'
 
 export default (router: Router): void => {
-  // eslint-disable-next-line @typescript-eslint/no-misused-promises
   router.post('/signup', adptRoute(makeSignUpController()))
 }

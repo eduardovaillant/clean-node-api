d1cb9d3253640984a78468e617493266f7a40ed6
eduardovaillant <eduardo.bvaillant@gmail.com>
2021-01-06 18:35:18 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -53,5 +53,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    test('Should return 401 if invalid credentials are provided', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'rodrigo.manguinho@gmail.com',
+          password: '123'
+        })
+        .expect(401)
+    })
   })
 })

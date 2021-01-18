944e7606d5b8b2230ea9eccd746b38bed0b0978b
eduardovaillant <eduardo.bvaillant@gmail.com>
2021-01-18 16:13:02 -0300

    feat: ensure AddSurveyController returns 204 on success

--- src/presentation/helpers/http/http-helper.ts
+++ src/presentation/helpers/http/http-helper.ts
@@ -1,6 +1,19 @@
 import { ServerError, UnauthorizedError } from '../../errors'
 import { HttpResponse } from '../../protocols/http'
 
+export const ok = (data: any): HttpResponse => (
+  {
+    statusCode: 200,
+    body: data
+  }
+)
+export const noContent = (): HttpResponse => (
+  {
+    statusCode: 204,
+    body: null
+  }
+)
+
 export const badRequest = (error: Error): HttpResponse => (
   {
     statusCode: 400,
@@ -26,12 +39,5 @@ export const serverError = (error: Error): HttpResponse => (
   {
     statusCode: 500,
     body: new ServerError(error.stack)
-  }
-)
-
-export const ok = (data: any): HttpResponse => (
-  {
-    statusCode: 200,
-    body: data
   }
 )

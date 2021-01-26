bad9495cbead4a9029cab25298cb880478e8bb16
eduardovaillant <eduardo.bvaillant@gmail.com>
2021-01-26 16:52:33 -0300

    refactor: change adaptRoute name

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -1,7 +1,7 @@
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
 import { Request, Response } from 'express'
 
-export const adptRoute = (controller: Controller) => {
+export const adaptRoute = (controller: Controller) => {
   return async (req: Request, res: Response) => {
     const httpRequest: HttpRequest = {
       body: req.body
--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express'
 import { adaptMiddleware } from '../adapters/express-middleware-adapter'
-import { adptRoute } from '../adapters/express-route-adapter'
+import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeAddSurveyControler } from '../factories/controllers/survey/add-survey/add-survey-controller-factory'
 import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-factory'
 
 export default (router: Router): void => {
   const adminAuth = adaptMiddleware(makeAuthMiddleware('admin'))
-  router.post('/surveys', adminAuth, adptRoute(makeAddSurveyControler()))
+  router.post('/surveys', adminAuth, adaptRoute(makeAddSurveyControler()))
 }
